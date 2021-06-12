8295f83192f020d231e126a18b7ba0fa2f469b20
jachewz <50257390+jachewz@users.noreply.github.com>
2021-06-12 16:51:43 +1000

    Copied sign up page and components, made it main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Dashboard from "./dashboard/dashboard";
 import SignInPage from "./signInPage/signInPage";
+import SignUpPage from "./signUpPage/signUpPage";
 
 function App() {
   return (
     // <Dashboard />
-    <SignInPage />
+    //<SignInPage />
+    <SignUpPage />
   );
 }
 
---
+++ client/src/signUpPage/signUpPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignUp from "../components/signup/signup";
+// import Navbar from "../components/navbar/navbar";
+const SignUpPage = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
